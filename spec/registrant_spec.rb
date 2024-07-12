ed79e7c3b5a1dc5c0f62dd1e48ec72942d7f7a62
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-12 17:05:38 -0600

    fix: registrant_2 typo. add new instance

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -4,7 +4,8 @@ require './lib/registrant'
 RSpec.describe Registrant do
     before(:each) do
      @registrant_1 = Registrant.new('Bruce', 18, true)
-     @registrant_2 = Registrant.new('Penny', 15)
+     @registrant_2 = Registrant.new('Penny', 16)
+     @registrant_3 = Registrant.new('Tucker', 15)
     end
     describe '#initialize' do
         it 'can initialize' do
