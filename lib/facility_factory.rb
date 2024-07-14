8d2b1fc56fedb48acd3f98f033edcbb86a3763cc
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-14 15:37:18 -0600

    fix: address in data has 2 underscores

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -8,7 +8,7 @@ class FacilityFactory
         dmv_locations.map do |location|
             location_data = {}
 
-            location_data[:address] = "#{location[:address_li]} #{location[:address_1]} #{location[:city]} #{location[:state]} #{location[:zip]}"
+            location_data[:address] = "#{location[:address_li]} #{location[:address__1]} #{location[:city]} #{location[:state]} #{location[:zip]}"
             location_data[:phone] = location[:phone]
             location_data[:name] = location[:dmv_office]
 
