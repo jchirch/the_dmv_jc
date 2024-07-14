71ad78995e41b69df533f07561ca0350fd462434
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-14 16:07:54 -0600

    test: test block for ny

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -3,6 +3,7 @@ require './lib/facility_factory'
 require './lib/dmv_data_service'
 
 RSpec.describe FacilityFactory do
+    describe '#create_co_facility' do
     before(:each) do
         @co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations 
         @factory = FacilityFactory.new
@@ -13,7 +14,7 @@ RSpec.describe FacilityFactory do
     end
 
     it 'facility has correct attributes' do
-        create_a_facility = @factory.create_facility(@co_dmv_office_locations)[1]
+        create_a_facility = @factory.create_co_facility(@co_dmv_office_locations)[1]
 
         expect(create_a_facility.phone).to eq("(720) 865-4600")
         expect(create_a_facility.name).to eq("DMV Northeast Branch")
@@ -21,6 +22,18 @@ RSpec.describe FacilityFactory do
     end
 
     it 'converts all input data to same ammount of facility objects' do
-        expect(@co_dmv_office_locations.length).to eq(@factory.create_facility(@co_dmv_office_locations).length)
+        expect(@co_dmv_office_locations.length).to eq(@factory.create_co_facility(@co_dmv_office_locations).length)
+    end
+    end
+    
+    describe '#create_ny_facility' do
+    before(:each) do
+        @ny_dmv_office_locations = DmvDataService.new.ny_dmv_office_locations
+        @factory = FacilityFactory.new
+    end
+    it 'exists' do
+        expect(@factory).to be_an_instance_of(FacilityFactory)
+    end
+    
     end
 end
