04e88b4caba597bbc3f7e51d6c151a38e5c2e15a
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-14 15:29:14 -0600

    feat: create facility method

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -4,5 +4,15 @@ class FacilityFactory
 
     end
 
-    
+    def create_facility(dmv_locations)
+        dmv_locations.map do |location|
+            location_data = {}
+
+            location_data[:address] = "#{location[:address_li]} #{location[:address_1]} #{location[:city]} #{location[:state]} #{location[:zip]}"
+            location_data[:phone] = location[:phone]
+            location_data[:name] = location[:dmv_office]
+
+            Facility.new(location_data)
+        end
+    end
 end
