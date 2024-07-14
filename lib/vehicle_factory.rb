dcf98c1cf1df1406013e0522d466b99ff9feb38b
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-14 11:55:05 -0600

    fix: removed commas in create vehicle method

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,5 +1,3 @@
-require 'dmv_data_service'
-
 class VehicleFactory
 
     def initialize
@@ -9,53 +7,53 @@ class VehicleFactory
     def create_vehicles(dmv_registrations)
         dmv_registrations.map do |registration|
             vehicle_data = {}
-            vehicle_data[:vin] = registration[:vin_1_10],
-            vehicle_data[:year] = registration[:model_year],
-            vehicle_data[:make] = registration[:make],
-            vehicle_data[:model] = registration[:model],
+           
+            vehicle_data[:vin] = registration[:vin_1_10]
+            vehicle_data[:year] = registration[:model_year]
+            vehicle_data[:make] = registration[:make]
+            vehicle_data[:model] = registration[:model]
             vehicle_data[:engine] = :ev
+            
             # vehicle_data[:registration_date] = registration[:transaction_date],
             # do registration date?
             # dont do plate type
-            # repeat for all desired
             Vehicle.new(vehicle_data)
-        end
-
+        end 
     end
-
-
 end
 
-# :electric_vehicle_type,
-#  :vin_1_10,
-#  :dol_vehicle_id,
-#  :model_year,
-#  :make,
-#  :model,
-#  :vehicle_primary_use,
-#  :electric_range,
-#  :odometer_reading,
-#  :odometer_code,
-#  :new_or_used_vehicle,
-#  :sale_price,
-#  :base_msrp,
-#  :transaction_type,
-#  :transaction_date,
-#  :transaction_year,
-#  :county,
-#  :city,
-#  :state_of_residence,
-#  :zip,
-#  :hb_2042_clean_alternative_fuel_vehicle_cafv_eligibility,
-#  :meets_2019_hb_2042_electric_range_requirement,
-#  :meets_2019_hb_2042_sale_date_requirement,
-#  :meets_2019_hb_2042_sale_price_value_requirement,
-#  :_2019_hb_2042_battery_range_requirement,
-#  :_2019_hb_2042_purchase_date_requirement,
-#  :_2019_hb_2042_sale_price_value_requirement,
-#  :electric_vehicle_fee_paid,
-#  :transportation_electrification_fee_paid,
-#  :hybrid_vehicle_electrification_fee_paid,
-#  :census_tract_2020,
-#  :legislative_district,
-#  :electric_utility
+# {
+#     "electric_vehicle_type": "Battery Electric Vehicle (BEV)",
+#     "vin_1_10": "3C3CFFGE5F",
+#     "dol_vehicle_id": "242052954",
+#     "model_year": "2015",
+#     "make": "FIAT",
+#     "model": "500",
+#     "vehicle_primary_use": "Passenger",
+#     "electric_range": "87",
+#     "odometer_reading": "0",
+#     "odometer_code": "Odometer reading is not collected at time of renewal",
+#     "new_or_used_vehicle": "Used",
+#     "sale_price": "0",
+#     "base_msrp": "0",
+#     "transaction_type": "Registration Renewal",
+#     "transaction_date": "2022-09-26T00:00:00.000",
+#     "transaction_year": "2022",
+#     "county": "Whatcom",
+#     "city": "EVERSON",
+#     "state_of_residence": "WA",
+#     "zip": "98247",
+#     "hb_2042_clean_alternative_fuel_vehicle_cafv_eligibility": "HB 2042 Eligibility Requirements not met",
+#     "meets_2019_hb_2042_electric_range_requirement": true,
+#     "meets_2019_hb_2042_sale_date_requirement": false,
+#     "meets_2019_hb_2042_sale_price_value_requirement": false,
+#     "_2019_hb_2042_battery_range_requirement": "Battery range requirement is met",
+#     "_2019_hb_2042_purchase_date_requirement": "This transaction type is not eligible for the tax exemption",
+#     "_2019_hb_2042_sale_price_value_requirement": "This transaction type is not eligible for the tax exemption",
+#     "electric_vehicle_fee_paid": "Yes",
+#     "transportation_electrification_fee_paid": "Yes",
+#     "hybrid_vehicle_electrification_fee_paid": "No",
+#     "census_tract_2020": "53073010701",
+#     "legislative_district": "42",
+#     "electric_utility": "PUGET SOUND ENERGY INC||PUD NO 1 OF WHATCOM COUNTY"
+#   }
