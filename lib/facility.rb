cafb18584a6a0fa5b927a07d774eb66a9090d767
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-15 21:26:37 -0600

    refactor: remove redundant booleans

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,5 +1,10 @@
 class Facility
-  attr_reader :name, :address, :phone, :services, :registered_vehicles, :collected_fees
+  attr_reader :name, 
+              :address, 
+              :phone, 
+              :services, 
+              :registered_vehicles, 
+              :collected_fees
 
   def initialize(facility_details)
     @name = facility_details[:name]
@@ -15,38 +20,28 @@ class Facility
   end
 
   def register_vehicle(vehicle)
-    if @services.include?('Vehicle Registration')
+    return unless @services.include?('Vehicle Registration')
       vehicle.set_registration_date
       vehicle.set_plate_type
       @collected_fees += vehicle.plate_fees
       @registered_vehicles << vehicle
-    end
   end
 
-    def administer_written_test(registrant)
-      return false unless @services.include?('Written Test')
-      if registrant.age >= 16 && registrant.permit? == true
-        registrant.pass_written
-      else
-        false
-      end
-    end
+  def administer_written_test(registrant)
+    return false unless @services.include?('Written Test')
+    return false unless registrant.age >= 16 && registrant.permit?
+    registrant.pass_written
+  end
 
-    def administer_road_test(registrant)
-      return false unless @services.include?('Road Test')
-      if administer_written_test(registrant) == true
-        registrant.pass_road
-      else
-        false
-      end
-    end
+  def administer_road_test(registrant)
+    return false unless @services.include?('Road Test')
+    return false unless administer_written_test(registrant)
+    registrant.pass_road
+  end
   
-    def renew_drivers_license(registrant)
-      return false unless @services.include?('Renew License')
-      if registrant.license? == true && administer_road_test(registrant) == true
-        registrant.renew_license
-      else
-        false
-      end
-    end
+  def renew_drivers_license(registrant)
+    return false unless @services.include?('Renew License')
+    return false unless registrant.license? == true && administer_road_test(registrant)
+    registrant.renew_license
+  end
 end
